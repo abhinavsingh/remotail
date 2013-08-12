89a49fa1cd085aa45ff66872a8560bc4992ded46
Abhinav Singh <mailsforabhinav@gmail.com>
2013-08-12 21:40:21 +0530

    first commit

---
+++ populate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Populates provided local file paths with some data 
+## to be used for development of remotail.py
+## 
+## Usage: ./populate /file/path.log /another/file/path.log ...
+
+if [ "$#" == 0 ]; then
+	echo "Atleast one filepath is required."
+	exit 1
+fi
+
+while true; do
+	NOW=$(date)
+	for f in "$@"; do
+    	echo $NOW > $f
+    done
+    sleep 0.5
+done
+
+exit 0
